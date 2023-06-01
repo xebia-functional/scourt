d4886d250ee7f6d51de74959f948e77f2112afdc
Diego E. Alonso <diego.e.a@47deg.com>
2023-06-01 13:14:57 +0100

    rename folders, sbt projects

---
+++ examples/src/main/scala/examples/TwoCurriedArgumentsTwoContinuations.scala
@@ -0,0 +1,10 @@
+package examples
+
+import continuations.Suspend
+import continuations.jvm.internal.SuspendApp
+
+@main def TwoCurriedArgumentsTwoContinuations =
+  def twoCurriedArgumentsTwoContinuations(x: Int)(y: Int)(using Suspend): Int =
+    summon[Suspend].shift[Unit] { continuation => continuation.resume(println(x + y)) }
+    summon[Suspend].shift[Int] { continuation => continuation.resume(3) }
+  println(SuspendApp(twoCurriedArgumentsTwoContinuations(1)(2)))
