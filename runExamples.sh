xebia-functional/Unwrapped

update the call site when we create the abstract class and call at the same place

runExamples.sh
@@ -53,4 +53,5 @@ continuationsPluginExample/runMain examples.ZeroArgumentsCodeBefore; \
 continuationsPluginExample/runMain examples.OneArgumentsSingleResumeContinuations; \
 continuationsPluginExample/runMain examples.OneFunctionArgumentTwoContinuationsTwoResumes; \
 continuationsPluginExample/runMain examples.TwoArgumentsZeroContinuations; \
-continuationsPluginExample/runMain examples.NonCompanionObjectContinuations"
+continuationsPluginExample/runMain examples.AbstractClassContinuations; \
+continuationsPluginExample/runMain examples.NonCompanionObjectContinuations;"

xebia-functional/scourt

runExamples.sh
@@ -53,4 +53,5 @@ examples/runMain examples.ZeroArgumentsCodeBefore; \
 examples/runMain examples.OneArgumentsSingleResumeContinuations; \
 examples/runMain examples.OneFunctionArgumentTwoContinuationsTwoResumes; \
 examples/runMain examples.TwoArgumentsZeroContinuations; \
+examples/runMain examples.AbstractClassContinuations; \
 examples/runMain examples.NonCompanionObjectContinuations"